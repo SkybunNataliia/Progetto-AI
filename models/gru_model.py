28f098b65df69b84b434c4ae905bd100662ed9f8
Nataliia Skybun <nataliia.skybun@studio.unibo.it>
2025-06-22 22:03:48 +0200

    Add: gru model

--- models/gru_model.py
+++ models/gru_model.py
@@ -1,15 +1,14 @@
 import torch
 import torch.nn as nn
 
-class LSTMModel(nn.Module):
+class GRUModel(nn.Module):
     def __init__(self, input_size, hidden_size=64, num_layers=2, dropout=0.2):
-        super(LSTMModel, self).__init__()
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, dropout=dropout)
+        super(GRUModel, self).__init__()
+        self.gru = nn.GRU(input_size, hidden_size, num_layers, batch_first=True, dropout=dropout)
         self.fc = nn.Linear(hidden_size, 1)
 
     def forward(self, x):
-        # x: [batch, seq_len, features]
-        out, (hn, cn) = self.lstm(x)
+        out, hn = self.gru(x)
         out = out[:, -1, :]
         out = self.fc(out)
         return out.squeeze(1)
