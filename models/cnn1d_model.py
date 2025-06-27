91b7dfc3403441b2acb84aa7c127537457afa496
Nataliia Skybun <nataliia.skybun@studio.unibo.it>
2025-06-27 14:59:49 +0200

    Fix: cnn model

--- models/cnn1d_model.py
+++ models/cnn1d_model.py
@@ -2,19 +2,24 @@ import torch
 import torch.nn as nn
 
 class CNN1DModel(nn.Module):
-    def __init__(self, input_size, num_channels=[32, 64], kernel_size=3, dropout=0.2):
+    def __init__(self, input_size, num_filters=[32, 64], kernel_size=3, stride=1, padding=0, dropout=0.2):
         super(CNN1DModel, self).__init__()
         layers = []
         in_channels = input_size
 
-        for out_channels in num_channels:
-            layers.append(nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size, padding=kernel_size//2))
+        for out_channels in num_filters:
+            layers.append(nn.Conv1d(
+                in_channels, out_channels,
+                kernel_size=kernel_size,
+                stride=stride,
+                padding=padding
+            ))
             layers.append(nn.ReLU())
             layers.append(nn.Dropout(dropout))
             in_channels = out_channels
 
         self.conv = nn.Sequential(*layers)
-        self.fc = nn.Linear(num_channels[-1], 1)
+        self.fc = nn.Linear(num_filters[-1], 1)
 
     def forward(self, x):
         # x: [batch, seq_len, features]
