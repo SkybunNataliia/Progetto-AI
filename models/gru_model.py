988c860467761b968fbbeb25356dcbb2cff3f42c
Nataliia Skybun <nataliia.skybun@studio.unibo.it>
2025-07-02 21:42:42 +0200

    Reverte: GRU model to initial state due to performance degradation

--- models/gru_model.py
+++ models/gru_model.py
@@ -4,12 +4,10 @@ class GRUModel(nn.Module):
     def __init__(self, input_size, hidden_size=64, num_layers=2, dropout=0.2):
         super(GRUModel, self).__init__()
         self.gru = nn.GRU(input_size, hidden_size, num_layers, batch_first=True, dropout=dropout)
-        self.dropout = nn.Dropout(dropout)
         self.fc = nn.Linear(hidden_size, 1)
 
     def forward(self, x):
         out, _ = self.gru(x)
         out = out[:, -1, :]
-        out = self.dropout(out)
         out = self.fc(out)
         return out.squeeze(1)
